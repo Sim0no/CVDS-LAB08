55b8fc913db0bdccfd81c1ad6c13425013720a58
unknown <2152805@labinfo.is.escuelaing.edu.co>
2019-10-05 09:49:18 -0500

    Agregando Funcionalidades

--- src/main/java/edu/eci/cvds/sampleprj/dao/mybatis/XXItemDAO.java
+++ src/main/java/edu/eci/cvds/sampleprj/dao/mybatis/XXItemDAO.java
@@ -7,6 +7,7 @@ package edu.eci.cvds.sampleprj.dao.mybatis;
 import edu.eci.cvds.sampleprj.dao.ItemDAO;
 import edu.eci.cvds.samples.entities.Item;
 import edu.eci.cvds.sampleprj.dao.PersistenceException;
+import java.util.List;
 /**
  *
  * @author 2152805
@@ -20,6 +21,11 @@ public class XXItemDAO implements ItemDAO{
 
     @Override
     public Item load(int id) throws PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Item> load() throws PersistenceException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
